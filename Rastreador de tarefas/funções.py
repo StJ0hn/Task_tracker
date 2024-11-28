d19c8d49a6552e3b62711e619b6e6fc189032119
John Miguel <jonhmiguel132@gmail.com>
2024-11-28 20:08:24 -0300

    Correção da função de adicionar tarefas

--- Rastreador de tarefas/funções.py
+++ Rastreador de tarefas/funções.py
@@ -1,11 +1,24 @@
 import typer
+import json
+from pathlib import Path
 
-
-lista_de_tarefas = []
+caminho_do_arquivo = "task.json"
 #função para adicionar tarefas:
 def adicao_de_tarefas(tarefa: str):
-    lista_de_tarefas.append(tarefa)
-    print(f"A tarefa: {tarefa} foi adicionada com sucesso!")
+    if Path(caminho_do_arquivo).exists():
+        with open(caminho_do_arquivo, "r") as f:
+            tarefas = json.load(f)
+    else:
+        tarefas = []
+
+    #Adicionar a nova tarefa no arquivo json
+    tarefas.append(tarefa)
+
+    #salvar as tarefas de volta no arquivo json
+    with open(caminho_do_arquivo, "w") as f:
+        json.dump(tarefas, f, indent=4)
+    
+    print(f"A tarefa '{tarefa}' foi adicionada com sucesso!")
 
 #Criando a interface de linha de comando
 if __name__ == "__main__":
